7664c8b60b394b5697fb065fb47cadf4c881c266
Margo584 <mfirs.mara666@gmail.com>
2023-04-19 12:22:48 +0300

    Добавила новые файлы

---
+++ hw3.js
@@ -0,0 +1,95 @@
+let password = '444';
+let inputPassword = prompt('Введите пароль');
+
+if (inputPassword === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}
+
+const c = 0;
+c >= 0 && c <= 10 ?
+console.log('Верно') : console.log('Неверно');
+
+const n = 10;
+n >= 0 && n <= 10 ?
+console.log('Верно') : console.log('Неверно');
+
+const i = -3;
+i >= 0 && i <= 10 ?
+console.log('Верно') : console.log('Неверно');
+
+const p = 10;
+p >= 0 && p <= 10 ?
+console.log('Верно') : console.log('Неверно');
+
+const d = 45;
+const e = 130;
+d <= e || d >= e ?
+console.log('Верно') : console.log('Неверно');
+
+let a = Number ('2');
+let b = Number ('3');
+alert(a + b);
+
+let monthNumbe = Number(prompt('Введите число от 1 до 12'));
+
+switch (monthNumbe) {
+    case 12 :
+    case 1:
+    case 2:
+        console.log('Зима')
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Осень')
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Лето')
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Весна')
+        break;
+    default:
+        console.log('Такого числа нет');
+        break;
+}
+
+let someNumber = prompt('Пожалуйста введие любое чило');
+if (isNaN(someNumber)) {
+    alert('это не число');
+} else {
+    if ((someNumber % 2) === 0) {
+        alert('четное число');
+    } else {
+        alert('число не четное');
+    }
+}
+
+let clientOS = Number(prompt('Пожалуйста, укажите 0, если вы пользователь IOS, и 1, если вы пользователь Android'));
+let currentYear = Number(prompt('Пожалуйста, укажите год выпуска вашего устройства'));
+
+if (clientOS === 0 && currentYear >= 2015) {
+    console.log('Установите версию приложения для iOS по ссылке: ссылка');
+} else if (clientOS === 0 && currentYear < 2015 && currentYear >= 2009)  {
+    console.log('Установите облегченную версию приложения для iOS по ссылке: ссылка');
+} else if (clientOS === 1 && currentYear >= 2015)  {
+    console.log('Установите версию приложения для Android по ссылке: ссылка');
+} else if (clientOS === 1 && currentYear < 2015 && currentYear >= 2008)  {
+    console.log('Установите облегченную версию приложения для Android по ссылке: ссылка');
+} else {
+    if (clientOS === 0 && clientOS === 1) {
+        console.log('Извините, но мы не работаем с другими OS, кроме IOS или Android'); 
+} 
+    if (clientOS === 0 && currentYear < 2009) {
+        console.log('Версии IOS такого года нет, потому что первый Iphone был выпущен в 2009 году');
+}
+    else if (clientOS === 0 && currentYear < 2008) {
+        console.log('Версии Android такого года нет, потому что первый Android был выпущен в 2008 году');
+}
+}
---
+++ script2.js
@@ -0,0 +1,57 @@
+let meaning = 10;
+alert (10);
+let number = 20;
+alert (20);
+
+
+const year = 2007;
+alert(year);
+
+const product = "Брендон Айком";
+alert(product);
+
+let j = 10;
+let up = 2;
+let f = j + up;
+alert(f);
+
+let w = 10;
+let c = 2;
+let d = w / c;
+alert(d);
+
+let result = 2**5;
+alert(result);
+
+let a = 9;
+let b = 2;
+let s = 9 / 2;
+alert(s);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+const user = {
+    name: `Margo`,
+    number: 24,
+    isAdmin: true,
+};
+    user['city of residence']='Ivanovo'
+    user.age= 24;
+    delete user['city of residence'];
+let info = prompt(`Какую информацию хотите узнать о пользователе?`, `Укажите name, age или isAdmin`);
+alert(user[info]);
+console.log(user[info]);
+
+let userName = prompt(`Как вас зовут?`)
+let greeting= `Привет, ${userName}!`;
+alert(greeting)
